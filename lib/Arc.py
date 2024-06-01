d77fe8c710ab89dcb745b9d86c7798056f73da4a
Flacko-Jodye <94548608+Flacko-Jodye@users.noreply.github.com>
2024-06-01 13:48:48 +0200

    asd

--- lib/Arc.py
+++ lib/Arc.py
@@ -1,10 +1,30 @@
+# https://github.com/williamfiset/Algorithms/blob/master/src/main/java/com/williamfiset/algorithms/graphtheory/networkflow/FordFulkersonDFSAdjacencyMatrix.java
+
+
+import json
+# Load the data
+with open('C:/Users/fabia/OneDrive/Dokumente/Master_FU/Semester 2/Netzwerke/F&F/F-F/Data/chvatal_small_transformed_v2.json', 'r') as infile:
+    data = json.load(infile)
+
+# Anzahl der Knoten
+num_nodes = len(data['nodes'])
+
+print(f"The number of nodes is {num_nodes}")
+
+# Klasse erstellen
+
 class Arc:
-    def __init__(self, from_node, to_node, cost, lower_bound, upper_bound):
+    def __init__(self, from_node, to_node, arc_flow, lower_bound, upper_bound, source = False, target = False):
         self.from_node = from_node # i
         self.to_node = to_node # j
-        self.cost = cost # bzw. Distanz
-        self.lower_bound = lower_bound
+        self.arc_flow = arc_flow # FLow
+        self.lower_bound = lower_bound # Redundant?
         self.upper_bound = upper_bound
+        self.source = source
+        self.target = target
+
+
+
         # Für FF später
         # self.flow = 0
         # self.reverse = None
--- lib/transformation.py
+++ lib/transformation.py
@@ -6,6 +6,12 @@ data = json.load(open('C:/Users/fabia/OneDrive/Dokumente/Master_FU/Semester 2/Ne
 data['nodes']['source'] = {'demand': 0}
 data['nodes']['target'] = {'demand': 0}
 
+# Umbenennen der Arcs
+#   upper bound der arcs werden zum flow
+
+for arc in data["arcs"]:
+    arc["arc_flow"] = arc["upper_bound"]
+
 for node, x in data['nodes'].items():
     if x['demand'] < 0:
         data['arcs'].append({
@@ -13,7 +19,7 @@ for node, x in data['nodes'].items():
             "to": node,
             "cost": 0,
             "lower_bound": 0,
-            "upper_bound": abs(x['demand'])
+            "arc_flow": abs(x['demand'])
         })
     elif x['demand'] > 0:
         data['arcs'].append({
@@ -21,9 +27,9 @@ for node, x in data['nodes'].items():
             "to": "target",
             "cost": 0,
             "lower_bound": 0,
-            "upper_bound": abs(x['demand'])
+            "arc_flow": abs(x['demand'])
         })
 
 # Transformierte Daten speichern
-with open('C:/Users/fabia/OneDrive/Dokumente/Master_FU/Semester 2/Netzwerke/F&F/F-F/Data/chvatal_small_transformed.json', 'w') as outfile:
+with open('C:/Users/fabia/OneDrive/Dokumente/Master_FU/Semester 2/Netzwerke/F&F/F-F/Data/chvatal_small_transformed_v2.json', 'w') as outfile:
     json.dump(data, outfile, indent=4)
