2725b017684647a1e4ca05740ec7e029f2d9d1d6
Linlu Hu <linluh00@zedat.fu-berlin.de>
2024-06-11 21:13:11 +0200

    update

--- lib/lib_Gurobi/mf_solver.py
+++ lib/lib_Gurobi/mf_solver.py
@@ -7,47 +7,47 @@ from gurobipy import GRB
 def solve_mf (nodes, arcs, source, sink):
     model = gp.Model("max_flow")
 
-    # Set Gurobi log file
+    # Gurobi log file
     model.Params.LogFile = 'mf_gurobi.log'
     
-    # Enable verbose output
+    # Hird können wir die Ausgabe steuern
     model.Params.OutputFlag = 1
     model.Params.DisplayInterval = 1 
     model.Params.FeasibilityTol = 1e-9
     model.Params.OptimalityTol = 1e-9
 
-    # create variable for flow : 0<flow< capacity
+    # Hier erstellen wir Flowvalue : 0<flow< capacity(lb: lower bound, ub: upper bound/capacity, name: from i to j)
     flow = {}
     for arc in arcs:
         flow[arc['start'], arc['end']] = model.addVar(
             lb=0, ub=arc['capacity'], name=f"flow_{arc['start']}_{arc['end']}")
         
         
-    #constraints        
+    # constraints        
     for node in nodes:
         if node == source:
                 
             model.addConstr(
                 gp.quicksum(flow[arc['start'], arc['end']] for arc in arcs if arc['end'] == node) == 0,
                 name=f"flow_balance_{node}_in")
-                #for source: 
-                            #sum of flow in = 0
+                #Für "Source": die Summe der Flüsse, die in die Quelle fließen, ist 0
+                            
         elif node == sink:
 
             model.addConstr(
                 gp.quicksum(flow[arc['start'], arc['end']] for arc in arcs if arc['start'] == node) == 0,
                 name=f"flow_balance_{node}_out")
-                #for sink: 
-                            #flow out = flow from source to other nodes
+                #Für "Sink": die Summe der Flüsse, die aus der Senke fließen, ist 0
+
         else:
             model.addConstr(
                 gp.quicksum(flow[arc['start'], arc['end']] for arc in arcs if arc['end'] == node) ==
                 gp.quicksum(flow[arc['start'], arc['end']] for arc in arcs if arc['start'] == node),
                 name=f"flow_balance_{node}")
+                #Für alle anderen Knoten: die Summe der Flüsse, die in den Knoten fließen, ist gleich der Summe der Flüsse, die aus dem Knoten fließen
     
     
-    
-    # Set objective
+    # Zielfunktion: Maximierung des Flusses, der in die Senke fließt
     model.setObjective(
         gp.quicksum(flow[arc['start'], arc['end']] for arc in arcs if arc['end'] == sink),
         GRB.MAXIMIZE)
@@ -55,20 +55,29 @@ def solve_mf (nodes, arcs, source, sink):
     
     
     ############################### Iteration speichern########################################
+    # Nur wenn wir die Callback-Funktion verwenden, können wir die Iterationen speichern
+
     def my_callback(model, where):
         if where == GRB.Callback.SIMPLEX:
             iter_count = model.cbGet(GRB.Callback.SPX_ITRCNT)
             obj_val = model.cbGet(GRB.Callback.SPX_OBJVAL)
             print(f"Iteration: {iter_count}, Objective Value: {obj_val}")
-            # Write each iteration's information to a file
+
+            # Hier speichern wir jede Iterationen in eine Datei
+
             with open("mf_iterations_gurobi.log", "a") as f:
                 f.write(f"Iteration: {iter_count}, Objective Value: {obj_val}\n")
+                
     ###############################################################################################
+
+
     model.optimize(my_callback)
+
+
     if model.status == GRB.OPTIMAL: #GRB.OPTIMAL =" the optimization was successful"
         max_flow = model.objVal
         flow_values = {"arcs": [{"start": arc[0], "end": arc[1], "flow": flow[arc].X, "capacity": flow[arc].ub} for arc in flow]}
-                        #  flow[arc].X = flow value of each arc
+                            #  flow[arc].X = hier wird der Wert des Flusses zurückgegeben, damit wir wissen, wie viel Fluss durch die Kante fließt
         return max_flow, flow_values
     else:
         raise Exception("No optimal solution found")
--- lib/lib_Gurobi_MinCost/mcf_solver.py
+++ lib/lib_Gurobi_MinCost/mcf_solver.py
@@ -6,46 +6,61 @@ def solve_mcf (nodes, arcs):
     
     model = gp.Model("min-cost-problem")
 
-    # Set Gurobi log file
+   
     model.Params.LogFile = 'gurobi_ohne_Startl.log'
     
-    # Enable verbose output
+    #Wir können die Ausgabe steuern
     model.Params.OutputFlag = 1
     model.Params.DisplayInterval = 1 
     model.Params.FeasibilityTol = 1e-9
     model.Params.OptimalityTol = 1e-9
 
-    # create variable for flow : 0<flow< capacity(lb: lower bound, ub: upper bound, name: from i to j)
+    # Wir erstellen die Variable für den Fluss: 0<flow< capacity(lb: lower bound, ub: upper bound, name: from i to j)
     flow = {}
     for arc in arcs:
         flow[arc['from'], arc['to']] = model.addVar(
             lb=0, ub=arc['upper_bound'], name=f"flow_{arc['from']}_{arc['to']}")
-    # 0<=flow<=upper_bound
+            # 0<=flow<=upper_bound
 
 
     for node in nodes:
         model.addConstr(
             gp.quicksum(flow[arc['from'], arc['to']] for arc in arcs if arc['to'] == node) - gp.quicksum(flow[arc['from'], arc['to']] for arc in arcs if arc['from'] == node)==nodes[node]['demand'],
             name=f"flow_balance_{node}_in")
-    #flow_in - flow_out = demand
+            #flow_in - flow_out = demand
 
 
-    # Set the objective function
+    # Zielfunktion: hier sollen wir die gesamte Kosten minimieren.
     model.setObjective(
         gp.quicksum(flow[arc['from'], arc['to']] * arc['cost'] for arc in arcs ),
         GRB.MINIMIZE)
-    #minimize sum (flow*cost)
+     #minimize sum (flow*cost)
+
+############################### Iteration speichern########################################
+#Durch die Callback-Funktion können wir die Iterationen speichern
 
 
-    # Solve the model
-    model.optimize()#my_callback
+    def my_callback(model, where):
+        if where == GRB.Callback.SIMPLEX:
+            iter_count = model.cbGet(GRB.Callback.SPX_ITRCNT)
+            obj_val = model.cbGet(GRB.Callback.SPX_OBJVAL)
+            print(f"Iteration: {iter_count}, Objective Value: {obj_val}")
+
+            # Iterationen können dadurch gespeichert
+            with open("iterations.log", "a") as f:
+                f.write(f"Iteration: {iter_count}, Objective Value: {obj_val}\n")
+  
+
+        #########################################################
+
+    model.optimize(my_callback) # Hier wird die Callback-Funktion aufgerufen und Optimierung gestartet
 
     if model.status == GRB.OPTIMAL: #GRB.OPTIMAL =" the optimization was successful"
         min_cost = model.objVal
         flow_values = {"arcs": [{"start": arc[0], "end": arc[1], "flow": flow[arc].X, "capacity": flow[arc].ub} for arc in flow]}
-                        #  flow[arc].X = flow value of each arc
+                        #  flow[arc].X = The value of the variable in the solution
         return min_cost, flow_values
-    # Return the flow values
+
     else:
         raise Exception("No optimal solution found")
 
